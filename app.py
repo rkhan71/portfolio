f40db2332e47a02395448e59426a0a1fb9ff497e
Rayan Khan <rayan.ahkhan@gmail.com>
2022-07-17 15:13:26 +0300

    sets up the different pages with a layout template

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
