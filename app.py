7d03bf273e2dda7bff57358a5ee8b4fe4cf25d9b
Rayan Khan <rayan.ahkhan@gmail.com>
2022-07-17 20:40:42 +0300

    lets users send me emails through the contact page

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, redirect, request
+from mailjet_rest import Client
 
 app = Flask(__name__)
 
+# configure app, mostly stuff for mail
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
@@ -16,6 +18,49 @@ def about():
 def projects():
     return render_template("projects.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        if not request.form.get("name"):
+            return redirect("/")
+        if not request.form.get("email"):
+            return redirect("/")
+        if not request.form.get("message"):
+            return redirect("/")
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+        api_key = '3c7cbcf70fe0f8cc6f9b43fedb4c9502'
+        api_secret = 'f5da1f9005ddd5165317ef7a6f3d6478'
+        mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+        data = {
+        'Messages': [
+            {
+            "From": {
+                "Email": "portfoliobot1@gmail.com",
+                "Name": "Portfolio"
+            },
+            "To": [
+                {
+                "Email": "portfoliobot1@gmail.com",
+                "Name": "Portfolio"
+                },
+                {
+                    "Email": "rayan.ahkhan@gmail.com",
+                    "Name": "Rayan"
+                },
+                {
+                    "Email": email,
+                    "Name": name
+                }
+            ],
+            "Subject": "Subject",
+            "TextPart": message
+            }
+        ]
+        }
+        mailjet.send.create(data=data)
+        sent = True
+    else:
+        sent = False
+    return render_template("contact.html", sent=sent)
